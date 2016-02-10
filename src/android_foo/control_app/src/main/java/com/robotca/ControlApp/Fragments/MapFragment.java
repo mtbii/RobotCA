ff9f2c5baebfc7feb14df208f92d20e624389eb1
PlayerFour <speark@email.sc.edu>
2016-02-09 19:58:27 -0500

    Added functionality of showing the latitude and longitude of a tapped point on the map

--- src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Fragments/MapFragment.java
+++ src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Fragments/MapFragment.java
@@ -7,22 +7,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.robotca.ControlApp.Core.RobotGPSSub;
 import com.robotca.ControlApp.R;
 
+import org.osmdroid.ResourceProxy;
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.overlays.GroundOverlay;
+import org.osmdroid.bonuspack.overlays.MapEventsOverlay;
+import org.osmdroid.bonuspack.overlays.MapEventsReceiver;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
 
 
-public class MapFragment extends RosFragment {
+
+public class MapFragment extends RosFragment implements MapEventsReceiver {
 
     private RobotGPSSub robotGPSNode;
+    MyLocationNewOverlay myLocationOverlay;
+    MapView mapView;
+    MapEventsOverlay mapEventsOverlay;
 
     public MapFragment(){}
 
@@ -31,7 +43,7 @@ public class MapFragment extends RosFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_map, null);
-        MapView mapView = (MapView) view.findViewById(R.id.mapview);
+        mapView = (MapView) view.findViewById(R.id.mapview);
         mapView.setClickable(true);
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
@@ -57,53 +69,17 @@ public class MapFragment extends RosFragment {
 
         robotGPSNode = new RobotGPSSub();
 
-        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(getActivity(), robotGPSNode, mapView);
+        myLocationOverlay = new MyLocationNewOverlay(getActivity(), robotGPSNode, mapView);
+        mapEventsOverlay = new MapEventsOverlay(mapView.getContext(), this);
+
         myLocationOverlay.enableMyLocation();
         myLocationOverlay.enableFollowLocation();
 
         mapView.getOverlays().add(myLocationOverlay);
+        mapView.getOverlays().add(0, mapEventsOverlay);
+
         IMapController mapViewController = mapView.getController();
         mapViewController.setZoom(18);
-
-        //location.getLatitude();
-        //location.getLongitude();
-        //location.set();
-        //location.set(Location l);
-        //location.setLatitude();
-        //location.setLongitude();
-
-        /*mapView.getOverlays().add(myLocationOverlay);
-        IMapController mapViewController = mapView.getController();
-        mapViewController.setZoom(16);
-        double lat = location.getLatitude();
-        double lng = location.getLongitude();
-        double lattitude = (double)lat;
-        double longitude = (double)lng;
-        GeoPoint me = new GeoPoint(lat, lng);*/
-        //mapViewController.setCenter(me);
-
-        //Location location = null;
-        //int lat = (int) (location.getLatitude() * 1E6);
-        //int lng = (int) (location.getLongitude() * 1E6);
-
-        //GeoPoint point = new GeoPoint(lat, lng);
-        //mapView.getController().setCenter(point);
-
-        //mapView.getController().animateTo(point);
-        //mapView.invalidate();
-
-
-     /*public void followRobot(){
-         Location robot = null;
-         //setLocation(robot);
-         mGeoPoint.setLatitudeE6((int) (robotLocation.getLatitude() * 1E6));
-         mGeoPoint.setLongitudeE6((int) (robotLocation.getLongitude() * 1E6));
-         mMapController.animateTo(mGeoPoint);
-
-         if(mapView != null)  {
-             mapView.postInvalidate();
-         }
-     }*/
 
         nodeMainExecutor.execute(robotGPSNode, nodeConfiguration.setNodeName("android/ros_gps"));
         return view;
@@ -111,5 +87,33 @@ public class MapFragment extends RosFragment {
 
     public void shutdown(){
         nodeMainExecutor.shutdownNodeMain(robotGPSNode);
+    }
+
+    @Override
+    public boolean singleTapConfirmedHelper(GeoPoint geoPoint) {
+        Toast.makeText(mapView.getContext(), "Tapped on (" + geoPoint.getLatitude() + "," + geoPoint.getLongitude() + ")", Toast.LENGTH_LONG).show();
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.enableFollowLocation();
+
+        return true;
+    }
+
+    @Override
+    public boolean longPressHelper(GeoPoint geoPoint) {
+
+        //The (ResourceProxy) this line causes the long press error and I'm not sure why
+        //Working on that though
+        GroundOverlay myGroundOverlay = new GroundOverlay((ResourceProxy) this);
+        myGroundOverlay.setPosition(geoPoint);
+        myGroundOverlay.setImage(getResources().getDrawable(R.drawable.marker).mutate());
+        myGroundOverlay.setDimensions(75.0f);
+        mapView.getOverlays().add(myGroundOverlay);
+        Toast.makeText(mapView.getContext(), "Marked on (" + geoPoint.getLatitude() + "," + geoPoint.getLongitude() + ")", Toast.LENGTH_LONG).show();
+
+        //Doesn't seem to be working for the long press
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.enableFollowLocation();
+
+        return true;
     }
 }
