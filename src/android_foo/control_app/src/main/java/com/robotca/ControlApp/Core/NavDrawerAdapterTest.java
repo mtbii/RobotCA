9a781422316c7795f9f6b0af8ce8a1afe4e2d0b8
lightyz <lightyz@email.sc.edu>
2016-03-03 16:44:01 -0500

    running test on navdraweradapter, possibly helps cam with his tests

---
+++ src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Core/NavDrawerAdapterTest.java
@@ -0,0 +1,25 @@
+package com.robotca.ControlApp.Core;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by lightyz on 3/3/16.
+ */
+public class NavDrawerAdapterTest extends TestCase {
+
+    public void testGetView() throws Exception {
+
+    }
+
+    public void testNotifyDataSetChanged() throws Exception {
+
+    }
+
+    public void testGetItem() throws Exception {
+
+    }
+
+    public void testGetItemId() throws Exception {
+
+    }
+}
