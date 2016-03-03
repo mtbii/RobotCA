02bbe470a10527f2fcc4d97c03512ce50f22285e
Nathaniel Stone <nlstone@email.sc.edu>
2016-03-03 11:11:48 -0500

    Fixed minor bug that caused RobotInfos to be created with the same name.

--- src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Core/RobotInfo.java
+++ src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Core/RobotInfo.java
@@ -3,13 +3,18 @@ package com.robotca.ControlApp.Core;
 import android.support.annotation.NonNull;
 
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
 /**
+ * Container for information about connections to specific Robots.
+ *
  * Created by Michael Brunson on 1/23/16.
  */
-public class RobotInfo implements Comparable<RobotInfo>{
+public class RobotInfo implements Comparable<RobotInfo> {
+
     private static int robotCount = 1;
+
     private UUID id = UUID.randomUUID();
     private String name;
     private String masterUriString;
@@ -18,6 +23,12 @@ public class RobotInfo implements Comparable<RobotInfo>{
     private String cameraTopic;
     private String laserTopic;
 
+    @SuppressWarnings("unused")
+    private static final String TAG = "RobotInfo";
+
+    /**
+     * Default Constructor.
+     */
     public RobotInfo(){
         //id = UUID.randomUUID();
         name = "Robot" + robotCount++;
@@ -27,11 +38,20 @@ public class RobotInfo implements Comparable<RobotInfo>{
         laserTopic = "/scan";
     }
 
-    public RobotInfo(String mName, String mMasterUri) {
-        this.name = mName;
-        this.masterUriString = mMasterUri;
-    }
+//    public RobotInfo(String mName, String mMasterUri) {
+//        this.name = mName;
+//        this.masterUriString = mMasterUri;
+//    }
 
+    /**
+     * Creates a RobotInfo.
+     * @param id UUID
+     * @param name Name to show when displaying this RobotInfo
+     * @param masterUriString Master URI for this RobotInfo
+     * @param joystickTopic JoystickTopic name for this RobotInfo
+     * @param laserTopic LaserTopic name for this RobotInfo
+     * @param cameraTopic CameraTopic name for this RobotInfo
+     */
     public RobotInfo(UUID id, String name, String masterUriString, String joystickTopic, String laserTopic, String cameraTopic) {
         this.id = id;
         this.name = name;
@@ -41,56 +61,107 @@ public class RobotInfo implements Comparable<RobotInfo>{
         this.laserTopic = laserTopic;
     }
 
+    /**
+     * @return UUID of this RobotInfo
+     */
     public UUID getId(){return id;}
 
+    /**
+     * Sets the UUID of this RobotInfo
+     * @param id The new UUID
+     */
     public void setId(UUID id){ this.id = id; }
 
+    /**
+     * @return The JoystickTopic name of this RobotInfo
+     */
     public String getJoystickTopic() {
         return joystickTopic;
     }
 
+    /**
+     * Sets the JoystickTopic for this RobotInfo.
+     * @param joystickTopic The new JoystickTopic
+     */
     public void setJoystickTopic(String joystickTopic) {
         this.joystickTopic = joystickTopic;
     }
 
+    /**
+     * @return The CameraTopic of this RobotInfo
+     */
     public String getCameraTopic() {
         return cameraTopic;
     }
 
+    /**
+     * Sets the CameraTopic of this RobotInfo.
+     * @param cameraTopic The new CameraTopic
+     */
     public void setCameraTopic(String cameraTopic) {
         this.cameraTopic = cameraTopic;
     }
 
+    /**
+     * @return The LaserTopic of this RobotInfo
+     */
     public String getLaserTopic() {
         return laserTopic;
     }
 
+    /**
+     * Sets the LaserTopic of this RobotInfo.
+     * @param laserTopic The new LaserTopic
+     */
     public void setLaserTopic(String laserTopic) {
         this.laserTopic = laserTopic;
     }
 
+    /**
+     * @return The Master URI of this RobotInfo
+     */
     public String getMasterUri() {
         return masterUriString;
     }
 
+    /**
+     * Sets the Master URI of this RobotInfo.
+     * @param mMasterUri The new Master URI
+     */
     public void setMasterUri(String mMasterUri) {
         this.masterUriString = mMasterUri;
     }
 
+    /**
+     * @return The name of this RobotInfo
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of this RobotInfo.
+     * @param mName The new name
+     */
     public void setName(String mName) {
         this.name = mName;
     }
 
+    /**
+     * @return The URI of Master URI of this RobotInfo
+     */
     public URI getUri(){
         return URI.create(getMasterUri());
     }
 
+    /**
+     * Compares this RobotInfo to another based on UUID.
+     * @param another The other RobotInfo
+     * @return The comparison result
+     */
     @Override
     public int compareTo(@NonNull RobotInfo another) {
+
         if(this.getId() == null){
             return -1;
         }
@@ -104,5 +175,35 @@ public class RobotInfo implements Comparable<RobotInfo>{
         }
 
         return this.getId().compareTo(another.getId());
+    }
+
+    /**
+     * Determines the correct value for robotCount which
+     * @param list The list of loaded RobotInfos
+     */
+    static void resolveRobotCount(List<RobotInfo> list)
+    {
+//        Log.d(TAG, "resolveRobotCount(" + list + ")");
+
+        int max = 0;
+        int val;
+
+        for (RobotInfo info: list) {
+            if (info.getName().startsWith("Robot"))
+            {
+//                Log.d(TAG, "name = " + info.getName().substring(5));
+                try {
+                    val = Integer.parseInt(info.getName().substring(5));
+                }
+                catch (NumberFormatException e) {
+                    val = -1;
+                }
+
+                if (val > max)
+                    max = val;
+            }
+        }
+
+        robotCount = max + 1;
     }
 }
--- src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Core/RobotStorage.java
+++ src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Core/RobotStorage.java
@@ -13,13 +13,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Manages the loading and saving of RobotInfos from an Activity's shared preferences.
+ *
  * Created by Michael Brunson on 2/2/16.
  */
 public class RobotStorage {
+
     private static final String ROBOT_INFOS_KEY = "ROBOT_INFOS_KEY";
+
     private static List<RobotInfo> g_cRobotInfos = Lists.newArrayList();
     private static Gson m_oGson = new Gson();
 
+    /**
+     * Loads from the specified Activity's preferences.
+     * @param activity The Activity
+     */
     public static synchronized void load(Activity activity){
             SharedPreferences pref = activity.getPreferences(Context.MODE_PRIVATE);
             String defaultJson = m_oGson.toJson(new ArrayList<RobotInfo>());
@@ -28,29 +36,54 @@ public class RobotStorage {
             Type listOfRobotInfoType = new TypeToken<List<RobotInfo>>() {
             }.getType();
 
-            g_cRobotInfos = (List<RobotInfo>) m_oGson.fromJson(robotInfoJson, listOfRobotInfoType);
+        g_cRobotInfos = m_oGson.fromJson(robotInfoJson, listOfRobotInfoType);
+
+        RobotInfo.resolveRobotCount(g_cRobotInfos);
     }
 
+    /**
+     * @return The list of loaded RobotInfos
+     */
     public static synchronized List<RobotInfo> getRobots(){
         return g_cRobotInfos;
     }
 
+    /**
+     * Removes a loaded RobotInfo.
+     * @param activity The Activity from which to remove
+     * @param robot The RobotInfo to remove
+     * @return True if the specified RobotInfo was removed, false otherwise
+     */
     public static synchronized boolean remove(Activity activity, RobotInfo robot){
         boolean removed = g_cRobotInfos.remove(robot);
         save(activity);
         return removed;
     }
 
+    /**
+     * Removes a loaded RobotInfo.
+     * @param activity The Activity from which to remove
+     * @param index The index of the RobotInfo to remove
+     * @return True if the specified RobotInfo was removed, false otherwise
+     */
     public static synchronized RobotInfo remove(Activity activity, int index){
         RobotInfo removed = g_cRobotInfos.remove(index);
         save(activity);
         return removed;
     }
 
-    public static synchronized boolean update(Activity activity, RobotInfo robot){
+    /**
+     * Updates the specified RobotInfo in storage.
+     * @param activity The Activity
+     * @param robot The updated RobotInfo
+     * @return True if the specified RobotInfo was updated, false otherwise
+     */
+    public static synchronized boolean update(Activity activity, RobotInfo robot) {
         boolean updated = false;
-        for(int i = 0; i < g_cRobotInfos.size(); i++){
-            if(g_cRobotInfos.get(i).compareTo(robot) == 0){
+
+        for(int i = 0; i < g_cRobotInfos.size(); i++) {
+
+            if(g_cRobotInfos.get(i).compareTo(robot) == 0) {
                 g_cRobotInfos.set(i, robot);
                 save(activity);
                 updated = true;
@@ -60,16 +93,26 @@ public class RobotStorage {
         return updated;
     }
 
+    /**
+     * Adds a RobotInfo to the RobotStorage.
+     * @param activity The Activity containing the RobotStorage
+     * @param robot The new RobotInfo
+     * @return True if the RobotInfo was successfully added, false otherwise
+     */
     public static synchronized boolean add(Activity activity, RobotInfo robot){
         boolean added = g_cRobotInfos.add(robot);
         save(activity);
         return added;
     }
 
+    /**
+     * Saves the RobotStorage.
+     * @param activity The Activity in which to save the RobotStorage
+     */
     public static synchronized void save(Activity activity){
         String robotInfosJson = m_oGson.toJson(g_cRobotInfos);
         SharedPreferences.Editor editor = activity.getPreferences(Context.MODE_PRIVATE).edit();
         editor.putString(ROBOT_INFOS_KEY, robotInfosJson);
-        editor.commit();
+        editor.apply();
     }
 }
