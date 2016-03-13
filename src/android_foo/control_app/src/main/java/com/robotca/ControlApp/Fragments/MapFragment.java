6bce70cf93b3951f71354f87ee0331f09b61ed1d
PlayerFour <speark@email.sc.edu>
2016-03-13 19:16:21 -0400

    Added ability to switch the centering of map on either the tablet, or the robot. Use single tap for robot and a longpress to center on tablet.

--- src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Fragments/MapFragment.java
+++ src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Fragments/MapFragment.java
@@ -41,8 +41,8 @@ public class MapFragment extends Fragment implements MapEventsReceiver {
     MyLocationNewOverlay secondMyLocationOverlay;
     MapView mapView;
     MapEventsOverlay mapEventsOverlay;
+    IMapController mapViewController;
 
-    double distanceToTravel;
     ArrayList<GeoPoint> waypoints = new ArrayList<>();
 
     public MapFragment() {
@@ -52,11 +52,13 @@ public class MapFragment extends Fragment implements MapEventsReceiver {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
         View view = inflater.inflate(R.layout.fragment_map, null);
         mapView = (MapView) view.findViewById(R.id.mapview);
+        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         robotRecenterButton = (Button) view.findViewById(R.id.recenter);
-        robotRecenterButton.setOnClickListener(robotRecenterListener);
+        //robotRecenterButton.setOnClickListener(robotRecenterListener);
         //tabletRecenterButton = (Button) view.findViewById(R.id.tabletRecenter);
         //tabletRecenterButton.setOnClickListener(tabletRecenterListener);
 
@@ -71,48 +73,71 @@ public class MapFragment extends Fragment implements MapEventsReceiver {
         RobotGPSSub robotGPSNode = ((HUDFragment) getActivity().
                 getFragmentManager().findFragmentById(R.id.hud_fragment)).getRobotGPSNode();
 
+        // Location overlay using the robot's GPS
         myLocationOverlay = new MyLocationNewOverlay(getActivity(), robotGPSNode, mapView);
+        // Location overlay using Android's GPS
         secondMyLocationOverlay = new MyLocationNewOverlay(getActivity(), mapView);
         mapEventsOverlay = new MapEventsOverlay(mapView.getContext(), this);
 
+        // Allow GPS updates
         myLocationOverlay.enableMyLocation();
         secondMyLocationOverlay.enableMyLocation();
 
+        // Center on and follow the robot by default
         myLocationOverlay.enableFollowLocation();
 
         mapView.getOverlays().add(myLocationOverlay);
         mapView.getOverlays().add(secondMyLocationOverlay);
         mapView.getOverlays().add(0, mapEventsOverlay);
 
-        IMapController mapViewController = mapView.getController();
+        // Set up the Center button
+        robotRecenterButton.setFocusable(false);
+        robotRecenterButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            // Center or recenter on Android with a long press
+            public boolean onLongClick(View v) {
+
+                mapView.postInvalidate();
+                myLocationOverlay.disableFollowLocation();
+                mapView.postInvalidate();
+                Toast.makeText(mapView.getContext(), "Centered on you", Toast.LENGTH_SHORT).show();
+                secondMyLocationOverlay.enableFollowLocation();
+                mapView.postInvalidate();
+                return true;
+
+            }
+        });
+        robotRecenterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            // Center or recenter on robot with a tap
+            public void onClick(View v) {
+                secondMyLocationOverlay.disableFollowLocation();
+                mapView.postInvalidate();
+                Toast.makeText(mapView.getContext(), "Centered on the Robot", Toast.LENGTH_SHORT).show();
+                myLocationOverlay.enableFollowLocation();
+                mapView.postInvalidate();
+
+            }
+        });
+
+        mapViewController = mapView.getController();
         mapViewController.setZoom(18);
 
         return view;
     }
 
-    private OnClickListener robotRecenterListener = new OnClickListener() {
-        public void onClick(View v) {
-            secondMyLocationOverlay.disableFollowLocation();
-            myLocationOverlay.enableFollowLocation();
-        }
-    };
-
-    private OnClickListener tabletRecenterListener = new OnClickListener() {
-        public void onClick(View v) {
-            myLocationOverlay.disableFollowLocation();
-            secondMyLocationOverlay.enableFollowLocation();
-        }
-    };
-
 
     @Override
+    // Tell the user the coordinates of a tapped point on the map
     public boolean singleTapConfirmedHelper(GeoPoint geoPoint) {
-        Toast.makeText(mapView.getContext(), "Tapped on (" + geoPoint.getLatitude() + "," + geoPoint.getLongitude() + ")", Toast.LENGTH_LONG).show();
+        Toast.makeText(mapView.getContext(), "Tapped on (" + geoPoint.getLatitude() + "," +
+                geoPoint.getLongitude() + ")", Toast.LENGTH_LONG).show();
 
         return true;
     }
 
     @Override
+    // Place a marker using a long press
     public boolean longPressHelper(GeoPoint geoPoint) {
 
         GroundOverlay myGroundOverlay = new GroundOverlay(getActivity());
@@ -126,29 +151,114 @@ public class MapFragment extends Fragment implements MapEventsReceiver {
         waypoints.add(myLocationOverlay.getMyLocation());
         waypoints.add(geoPoint);
 
-        Toast.makeText(mapView.getContext(), "Marked on (" + geoPoint.getLatitude() + "," + geoPoint.getLongitude() + ")", Toast.LENGTH_LONG).show();
+        Toast.makeText(mapView.getContext(), "Marked on (" + geoPoint.getLatitude() + "," +
+                geoPoint.getLongitude() + ")", Toast.LENGTH_LONG).show();
 
         return true;
     }
 
-    public double distanceBetweenCoordinates(GeoPoint start, GeoPoint dest) {
-        distanceToTravel = 0;
-        int earthRadius = 6371000; // m
-        double dLat = Math.toRadians(dest.getLatitude() - start.getLatitude());
-        double dLon = Math.toRadians(dest.getLongitude() - dest.getLongitude());
-        double lat1 = Math.toRadians(start.getLatitude());
-        double lat2 = Math.toRadians(dest.getLatitude());
 
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        distanceToTravel = earthRadius * c;  // in meters
+    // Function to compute distance between 2 points as well as the angle (bearing) between them
+    private static void computeDistanceAndBearing(double lat1, double lon1,
+                                                  double lat2, double lon2, float[] results) {
+        // Based on http://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf
+        // using the "Inverse Formula" (section 4)
 
-        Toast.makeText(mapView.getContext(), "Waypoint is " + distanceToTravel + " meters away", Toast.LENGTH_LONG).show();
+        int MAXITERS = 20;
+        // Convert lat/long to radians
+        lat1 *= Math.PI / 180.0;
+        lat2 *= Math.PI / 180.0;
+        lon1 *= Math.PI / 180.0;
+        lon2 *= Math.PI / 180.0;
 
-        return distanceToTravel;
+        double a = 6378137.0; // WGS84 major axis
+        double b = 6356752.3142; // WGS84 semi-major axis
+        double f = (a - b) / a;
+        double aSqMinusBSqOverBSq = (a * a - b * b) / (b * b);
+
+        double L = lon2 - lon1;
+        double A = 0.0;
+        double U1 = Math.atan((1.0 - f) * Math.tan(lat1));
+        double U2 = Math.atan((1.0 - f) * Math.tan(lat2));
+
+        double cosU1 = Math.cos(U1);
+        double cosU2 = Math.cos(U2);
+        double sinU1 = Math.sin(U1);
+        double sinU2 = Math.sin(U2);
+        double cosU1cosU2 = cosU1 * cosU2;
+        double sinU1sinU2 = sinU1 * sinU2;
+
+        double sigma = 0.0;
+        double deltaSigma = 0.0;
+        double cosSqAlpha;
+        double cos2SM;
+        double cosSigma;
+        double sinSigma;
+        double cosLambda = 0.0;
+        double sinLambda = 0.0;
+
+        double lambda = L; // initial guess
+        for (int iter = 0; iter < MAXITERS; iter++) {
+            double lambdaOrig = lambda;
+            cosLambda = Math.cos(lambda);
+            sinLambda = Math.sin(lambda);
+            double t1 = cosU2 * sinLambda;
+            double t2 = cosU1 * sinU2 - sinU1 * cosU2 * cosLambda;
+            double sinSqSigma = t1 * t1 + t2 * t2; // (14)
+            sinSigma = Math.sqrt(sinSqSigma);
+            cosSigma = sinU1sinU2 + cosU1cosU2 * cosLambda; // (15)
+            sigma = Math.atan2(sinSigma, cosSigma); // (16)
+            double sinAlpha = (sinSigma == 0) ? 0.0 :
+                    cosU1cosU2 * sinLambda / sinSigma; // (17)
+            cosSqAlpha = 1.0 - sinAlpha * sinAlpha;
+            cos2SM = (cosSqAlpha == 0) ? 0.0 :
+                    cosSigma - 2.0 * sinU1sinU2 / cosSqAlpha; // (18)
+
+            double uSquared = cosSqAlpha * aSqMinusBSqOverBSq; // defn
+            A = 1 + (uSquared / 16384.0) * // (3)
+                    (4096.0 + uSquared *
+                            (-768 + uSquared * (320.0 - 175.0 * uSquared)));
+            double B = (uSquared / 1024.0) * // (4)
+                    (256.0 + uSquared *
+                            (-128.0 + uSquared * (74.0 - 47.0 * uSquared)));
+            double C = (f / 16.0) *
+                    cosSqAlpha *
+                    (4.0 + f * (4.0 - 3.0 * cosSqAlpha)); // (10)
+            double cos2SMSq = cos2SM * cos2SM;
+            deltaSigma = B * sinSigma * // (6)
+                    (cos2SM + (B / 4.0) *
+                            (cosSigma * (-1.0 + 2.0 * cos2SMSq) -
+                                    (B / 6.0) * cos2SM *
+                                            (-3.0 + 4.0 * sinSigma * sinSigma) *
+                                            (-3.0 + 4.0 * cos2SMSq)));
+
+            lambda = L +
+                    (1.0 - C) * f * sinAlpha *
+                            (sigma + C * sinSigma *
+                                    (cos2SM + C * cosSigma *
+                                            (-1.0 + 2.0 * cos2SM * cos2SM))); // (11)
+
+            double delta = (lambda - lambdaOrig) / lambda;
+            if (Math.abs(delta) < 1.0e-12) {
+                break;
+            }
+        }
+
+        float distance = (float) (b * A * (sigma - deltaSigma));
+        results[0] = distance;
+        if (results.length > 1) {
+            float initialBearing = (float) Math.atan2(cosU2 * sinLambda,
+                    cosU1 * sinU2 - sinU1 * cosU2 * cosLambda);
+            initialBearing *= 180.0 / Math.PI;
+            results[1] = initialBearing;
+            if (results.length > 2) {
+                float finalBearing = (float) Math.atan2(cosU1 * sinLambda,
+                        -sinU1 * cosU2 + cosU1 * sinU2 * cosLambda);
+                finalBearing *= 180.0 / Math.PI;
+                results[2] = finalBearing;
+            }
+        }
     }
-
 
 }
 
